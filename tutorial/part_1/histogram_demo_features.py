d7023f863974c90b3133e0e8a3892d46c0bc75ad
Roha Park <rohawolf@likelion.net>
2019-09-26 15:40:09 +0900

    add scipy library & histogram example

---
+++ tutorial/part_1/histogram_demo_features.py
@@ -0,0 +1,32 @@
+"""
+ref: https://computation.physics.utoronto.ca/tutorials/tutorial-part-1-first-steps-python-3/
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as ss
+
+
+np.random.seed(0)
+
+# example data
+mu = 100  # mean of distribution
+sigma = 15  # standard deviation of distribution
+x = mu + sigma * np.random.randn(437)
+
+num_bins = 50
+
+fig, ax = plt.subplots()
+
+# the histogram of the data
+n, bins, patches = ax.hist(x, num_bins, density=1)
+
+# add a 'best fit' line
+y = ss.norm.pdf(bins, mu, sigma)
+ax.plot(bins, y, '--')
+ax.set_xlabel('Smarts')
+ax.set_ylabel('Probability density')
+ax.set_title(r'Histogram of IQ: $\mu=100$, $\sigma=15$')
+
+# Tweak spacing to prevent clipping of ylabel
+fig.tight_layout()
+plt.show()
